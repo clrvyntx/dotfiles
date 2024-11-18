68c6a8d65cbbfd1327b4415dd7b473afd8394191
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2024-11-18 15:48:23 -0300

    Update status.sh

--- sway_minimal/sway/status.sh
+++ sway_minimal/sway/status.sh
@@ -1,48 +1,16 @@
 #!/bin/sh
- 
-common() {
-echo -n "{"
-echo -n "\"name\":\"$name\","
-echo -n "\"full_text\":\" $stat\","
-echo -n "},"
-}
- 
-battery0() {
-local name="battery0"
-prct=$(cat /sys/class/power_supply/BAT1/capacity)
-chrg=$(cat /sys/class/power_supply/BAT1/status)
-icon=" "
-case $chrg in
-"Charging")  icon="" ;;
-"Full") icon="" ;;
-esac 
-local stat=$icon\ $prct%
-common
-}
- 
-mydate() {
-local name="id_time"
-local stat=$(date "+%F %H:%M") 
-common
-}
- 
-volume() {
-local name="id_volume"
-vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d : -f 2)
-vol="$vol"
-local stat=${vol} 
-common
-}
- 
-echo '{ "version": 1 , "click_events":false}'
-echo '[[],'
- 
-while :
-do
-echo -n "["
-battery0
-volume
-mydate
-echo -n "],"
-sleep 1
-done
+
+date_formatted=$(date "+%F %H:%M")
+
+battery_info=$(upower --show-info $(upower --enumerate |\
+grep 'BAT') |\
+egrep "state|percentage" |\
+awk '{print $2}')
+
+audio_volume=$(amixer -M get Master |\
+awk '/Mono.+/ {print $6=="[off]" ?\
+$4" ": \
+$4" "}' |\
+tr -d [])
+
+echo $audio_volume $battery_info  $date_formatted
