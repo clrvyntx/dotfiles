48e37d571bccaa979c3cbc8026e829e1db5f1332
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2024-11-18 18:41:57 -0300

    Update status.sh

--- sway_minimal/sway/status.sh
+++ sway_minimal/sway/status.sh
@@ -6,10 +6,8 @@ date_calendar=$(date "+%F")
 
 battery_capacity=$(cat /sys/class/power_supply/BAT1/capacity)
 
-battery_status=$(cat /sys/class/power_supply/BAT1/status)
-
 audio_volume=$(wpctl get-volume @DEFAULT_SINK@ | cut -d : -f 2)
 
 mic_volume=$(wpctl get-volume @DEFAULT_SOURCE@ | cut -d : -f 2)
 
-echo  $mic_volume  $audio_volume  $battery_capacity% \($battery_status\)  $date_calendar  $date_time
+echo  $mic_volume  $audio_volume  $battery_capacity%  $date_calendar  $date_time
