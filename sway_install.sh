a003e08c39f4e45668e6c08e0c58b1744da95ac6
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2024-11-19 05:04:50 -0300

    Update sway_install.sh

--- sway_install.sh
+++ sway_install.sh
@@ -1,5 +1,5 @@
 fonts="noto-fonts noto-fonts-cjk noto-fonts-emoji otf-font-awesome"
-desktop="sway waybar swaybg swayidle swaylock gammastep dunst libnotify autotiling-rs polkit-gnome xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk xorg-xwayland qt5-wayland qt6-wayland"
+desktop="sway waybar swaybg swayidle swaylock gammastep mako libnotify autotiling-rs polkit-gnome xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk xorg-xwayland qt5-wayland qt6-wayland"
 utils="foot fuzzel firefox pcmanfm-gtk3 gvfs file-roller neovim wl-clipboard swayimg mpv grim slurp ufw materia-gtk-theme papirus-icon-theme"
 
 sudo pacman -S --needed $fonts $desktop $utils
