8f3b96de9f9901ee9fcb224a39510e77c49927ea
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-09-23 20:22:12 -0300

    Update sway_install.sh

--- sway_install.sh
+++ sway_install.sh
@@ -1,15 +1,12 @@
 fonts="noto-fonts noto-fonts-cjk noto-fonts-emoji otf-font-awesome"
 desktop="sway waybar swaybg swayidle swaylock gammastep mako libnotify autotiling-rs polkit-gnome xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk xorg-xwayland qt5-wayland qt6-wayland ly"
-utils="foot fuzzel firefox pcmanfm-gtk3 gvfs file-roller neovim wl-clipboard swayimg mpv grim slurp ufw materia-gtk-theme papirus-icon-theme"
+utils="foot fuzzel firefox pcmanfm-gtk3 gvfs file-roller mousepad wl-clipboard swayimg mpv grim slurp ufw materia-gtk-theme papirus-icon-theme"
 
 sudo pacman -S --needed $fonts $desktop $utils
 
 mkdir -p ~/.config
 cp -r ./dotfiles/* ~/.config
 cp -r ./sway/* ~/.config
-
-mkdir -p ~/.local/share/nvim/site/pack/plugins/start/nvim-scrollview
-mv ~/.config/nvim/nvim-scrollview ~/.local/share/nvim/site/pack/plugins/start/nvim-scrollview
 
 sudo systemctl enable ly.service
 sudo systemctl disable getty@tty2.service
