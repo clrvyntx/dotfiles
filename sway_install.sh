bd04f9bf390027037dd48bf4b379c1be000b0ce5
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2025-09-25 02:05:57 -0300

    Update sway_install.sh

--- sway_install.sh
+++ sway_install.sh
@@ -1,5 +1,5 @@
 fonts="noto-fonts noto-fonts-cjk noto-fonts-emoji otf-font-awesome"
-desktop="sway waybar swaybg swayidle swaylock gammastep mako libnotify autotiling-rs polkit-gnome xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk xorg-xwayland qt5-wayland qt6-wayland ly"
+desktop="sway waybar swaybg swayidle swaylock gammastep mako libnotify autotiling-rs polkit-gnome xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk xorg-xwayland qt5-wayland qt6-wayland"
 utils="foot fuzzel firefox pcmanfm-gtk3 gvfs file-roller neovim wl-clipboard swayimg mpv grim slurp ufw materia-gtk-theme papirus-icon-theme"
 
 sudo pacman -S --needed $fonts $desktop $utils
@@ -8,6 +8,4 @@ mkdir -p ~/.config
 cp -r ./dotfiles/* ~/.config
 cp -r ./sway/* ~/.config
 
-sudo systemctl enable ly.service
-sudo systemctl disable getty@tty2.service
 sudo ufw enable
