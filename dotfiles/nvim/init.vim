7beeff74cf7c4175ebe12d7570db8b9d5a69dde5
clrvyntx <73581070+clrvyntx@users.noreply.github.com>
2026-03-01 01:39:50 -0300

    Add files via upload

---
+++ dotfiles/nvim/init.vim
@@ -0,0 +1,7 @@
+aunmenu PopUp.How-to\ disable\ mouse
+aunmenu PopUp.-1-
+colorscheme industry
+set tabstop=4
+set expandtab
+set softtabstop=4
+set shiftwidth=4
---
+++ sway_install.sh
@@ -0,0 +1,11 @@
+fonts="noto-fonts noto-fonts-cjk noto-fonts-emoji otf-font-awesome"
+desktop="sway waybar swaybg swayidle swaylock gammastep mako libnotify autotiling-rs polkit-gnome xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk xorg-xwayland qt5-wayland qt6-wayland"
+utils="foot fuzzel firefox pcmanfm-gtk3 gvfs file-roller neovim wl-clipboard swayimg mpv grim slurp ufw materia-gtk-theme papirus-icon-theme"
+
+sudo pacman -S --needed $fonts $desktop $utils
+
+mkdir -p ~/.config
+cp -r ./dotfiles/* ~/.config
+mv ~/.config/bash_profile ~/.bash_profile
+
+sudo ufw enable
